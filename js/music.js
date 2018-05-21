25649b34949edd20dca2fae297dc3602f47ee5de
dkymore <33784402+dkymore@users.noreply.github.com>
2018-05-21 21:57:55 +0800

    Initial commit

---
+++ js/music.js
@@ -0,0 +1,20 @@
+var x = document.getElementById("myAudio1"); 
+
+function playAudio1()
+{ 
+	  x.play(); 
+} 
+
+var y = document.getElementById("myAudio2"); 
+
+function playAudio2()
+{ 
+	  y.play(); 
+} 
+
+var z = document.getElementById("myAudio3"); 
+
+function playAudio3()
+{ 
+	  z.play(); 
+} 
